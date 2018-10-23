7ff6eeafdacf6a6008e43e792a6bd3ff342e5e20
S.Zares <sergiozares@gmail.com>
2018-10-23 12:18:24 +0300

    Add config

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+	'viewDir' => MAKO_APPLICATION_PATH . '/resources/views',
+
+	'caheDir' => MAKO_APPLICATION_PATH . '/storage/views',
+
+    'fileExtension' => '.html.twig',
+
+];
