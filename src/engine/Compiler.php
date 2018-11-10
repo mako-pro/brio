994d97c0f28e10637441dfaf2b5969d150fc0e1b
S.Zares <sergiozares@gmail.com>
2018-11-11 00:04:38 +0200

    Work on Compiler class

--- src/engine/Compiler.php
+++ src/engine/Compiler.php
@@ -2,7 +2,10 @@
 
 namespace placer\brio\engine;
 
+use mako\utility\Str;
 use placer\brio\engine\generator\PHP as PhpGenerator;
+use placer\brio\engine\compiler\CompilerException;
+use placer\brio\engine\compiler\Tokenizer;
 use placer\brio\engine\helper\AST;
 use placer\brio\engine\helper\BH;
 
@@ -16,14 +19,21 @@ class Compiler
     private static $instance;
 
     /**
-     * Generator
+     * Generator instance
      *
-     * @var object
+     * @var placer\brio\engine\generator\PHP
      */
     protected $generator;
 
     /**
-     * Base temlate
+     * AST instance
+     *
+     * @var placer\brio\engine\helper\AST
+     */
+    protected $ast;
+
+    /**
+     * Base template
      *
      * @var array
      */
@@ -77,20 +87,6 @@ class Compiler
      * @var integer
      */
     protected $obstartNum = 0;
-
-    /**
-     * Append
-     *
-     * @var string
-     */
-    protected $append;
-
-    /**
-     * Prepend OP
-     *
-     * @var object
-     */
-    protected $prependOP;
 
     /**
      * Context at compile time
@@ -232,6 +228,63 @@ class Compiler
     }
 
     /**
+     * Do print
+     *
+     * @param  object         $code
+     * @param  array|object  $stmt
+     *
+     * @return void
+     */
+    public function doPrint(AST $code, $stmt)
+    {
+        $code->doesPrint = true;
+
+        if (self::$stripWhitespace && AST::is_str($stmt))
+        {
+            $stmt['string'] = preg_replace('/\s+/', ' ', $stmt['string']);
+        }
+
+        if ($this->obstartNum == 0)
+        {
+            $code->do_echo($stmt);
+            return;
+        }
+
+        $buffer = BH::hvar('buffer_' . $this->obstartNum);
+
+        $code->append($buffer, $stmt);
+    }
+
+    /**
+     * Get template name
+     *
+     * @return string Path to template file
+     */
+    public function getTemplateFile()
+    {
+        return $this->templateFile;
+    }
+
+    /**
+     *  Compile a file
+     *
+     *  @param string $file      File path
+     *  @param bool   $safe      Whether or not add check if the function is already defined
+     *  @param array  $context   Vars
+     *
+     *  @return string           Generated PHP code
+     */
+    public function compileFile(string $file, $safe = false, $context = [])
+    {
+        $this->templateFile  = $file;
+        $this->checkFunction = $safe;
+        $this->context       = $context;
+        $this->ast           = new AST;
+
+        return $this->compile();
+    }
+
+    /**
      * Get function name
      *
      * @param  string $name
@@ -274,76 +327,262 @@ class Compiler
     }
 
     /**
-     * Do print
+     *  Set base template
      *
-     * @param  object         $code
-     * @param  array|object  $stmt
+     *  @param array $base Base template as ['string' => 'template.name']
+     *
+     *  @return void
+     */
+    protected function setBaseTemplate($base = [])
+    {
+        $this->baseTemplate = $base;
+    }
+
+    /**
+     * Generate code to call base template
+     *
+     * @return object AST
+     */
+    protected function exprCallBaseTemplate()
+    {
+        return BH::hexec(
+            'placer\brio\Brio::load',
+            $this->baseTemplate,
+            $this->getScopeVariable(),
+            true,
+            BH::hvar('blocks')
+        );
+    }
+
+    /**
+     * Generate OpCode
+     *
+     * @param  array  $parsed
+     * @param  AST    &$ast
      *
      * @return void
      */
-    public function doPrint(AST $code, $stmt)
+    protected function generateOpCode(array $parsed, AST &$ast): void
     {
-        $code->doesPrint = true;
 
-        if (self::$stripWhitespace && AST::is_str($stmt))
+        foreach ($parsed as $op)
         {
-            $stmt['string'] = preg_replace('/\s+/', ' ', $stmt['string']);
+            if (! is_array($op))
+            {
+                continue;
+            }
+
+            if (! isset($op['operation']))
+            {
+                throw new CompilerException("Invalid parsed data: " . print_r($op, true));
+            }
+
+            if ($this->baseTemplate && $this->numBlocks == 0 && $op['operation'] != 'block')
+            {
+                continue;
+            }
+
+            $method = 'generateOp' . Str::underscored2camel($op['operation']);
+
+            if (! is_callable([$this, $method]))
+            {
+                throw new CompilerException("Missing Compiler method $method");
+            }
+
+            $this->$method($op, $ast);
         }
-
-        if ($this->obstartNum == 0)
-        {
-            $code->do_echo($stmt);
-            return;
-        }
-
-        $buffer = BH::hvar('buffer_' . $this->obstartNum);
-
-        $code->append($buffer, $stmt);
     }
 
     /**
-     * Get template name
+     * Exception handler for Base Operation
      *
-     * @return string Path to template file
+     * @return CompilerException
      */
-    private function getTemplateName()
+    protected function generateOpBase()
     {
-        return $this->templateFile;
+        throw new CompilerException("{% base %} can be only as first statement");
     }
 
     /**
-     *  Compile a file
+     * Include file
      *
-     *  @param string $file      File path
-     *  @param bool   $safe      Whether or not add check if the function is already defined
-     *  @param array  $context   Vars
+     * @param  array $details
+     * @param  AST  &$body
      *
-     *  @return string           Generated PHP code
+     * @return void
      */
-    public function compileFile(string $file, $safe = false, $context = [])
+    protected function generateOpInclude(array $details, &$body)
     {
-        $this->templateFile  = $file;
-        $this->checkFunction = $safe;
-        $this->context       = $context;
+        $this->doPrint(
+            $body,
+            BH::hexec(
+                'placer\brio\Brio::load',
+                $details[0],
+                $this->getScopeVariable(),
+                true,
+                BH::hvar('blocks')
+            )
+        );
+    }
 
-        $content = file_get_contents($file);
+    /**
+     * Check the current expr
+     *
+     * @param  array &$expr
+     *
+     * @return void
+     */
+    protected function checkExpr(array &$expr)
+    {
+        if (AST::is_expr($expr))
+        {
+            if ($expr['op_expr'] == 'in')
+            {
+                for ($id=0; $id<2; $id++)
+                {
+                    if ($this->varFilter($expr[$id]))
+                    {
+                        $expr[$id] = $this->getFilteredVar($expr[$id]['var_filter'], $var);
+                    }
+                }
 
-        return $this->compile($content, $file);
+                if (AST::is_str($expr[1]))
+                {
+                    $expr = BH::hexpr(
+                        BH::hexec(
+                            'strpos',
+                            $expr[1],
+                            $expr[0]
+                        ),
+                        '!==',
+                        false
+                    );
+                }
+                else
+                {
+                    $expr = BH::hexpr(
+                        BH::hexpr_cond(
+                            BH::hexec('is_array', $expr[1]),
+                            BH::hexec('array_search', $expr[0], $expr[1]),
+                            BH::hexec('strpos', $expr[1], $expr[0])
+                        ),
+                        '!==',
+                        false
+                    );
+                }
+            }
+
+            if (is_object($expr))
+            {
+                $expr = $expr->getArray();
+            }
+
+            $this->checkExpr($expr[0]);
+            $this->checkExpr($expr[1]);
+
+        }
+        elseif (is_array($expr))
+        {
+            if ($this->varFilter($expr))
+            {
+                $expr = $this->getFilteredVar($expr['var_filter'], $var);
+            }
+            elseif (isset($expr['args']))
+            {
+                foreach ($expr['args'] as &$v)
+                {
+                    $this->checkExpr($v);
+                }
+                unset($v);
+            }
+            else if (isset($expr['expr_cond']))
+            {
+                $this->checkExpr($expr['expr_cond']);
+                $this->checkExpr($expr['true']);
+                $this->checkExpr($expr['false']);
+            }
+        }
+    }
+
+    /**
+     * Check is set var filter
+     *
+     * @param  array $cmd
+     *
+     * @return bool
+     */
+    protected function varFilter(array $cmd)
+    {
+        return isset($cmd['var_filter']);
     }
 
     /**
      * Final compile
      *
-     * @param  string $content
-     * @param  string $file
-     *
      * @return string
      */
-    private function compile(string $content, string $file)
+    private function compile()
     {
-        // ...
+        $ast = $this->ast;
 
-        return $content;
+        $data = file_get_contents($this->templateFile);
+
+        $parsed = Tokenizer::init($this, $data);
+
+        if (isset($parsed[0]) && $parsed[0]['operation'] == 'base')
+        {
+            $base = $parsed[0][0];
+
+            $this->setBaseTemplate($base);
+
+            unset($parsed[0]);
+        }
+
+        $functionName = $this->getFunctionName($this->templateFile);
+
+        if ($this->checkFunction)
+        {
+            $ast->doIf(BH::hexpr(BH::hexec('function_exists', $functionName), '===', false));
+        }
+
+        $ast->comment("Generated from " . $this->templateFile);
+        $ast->declareFunction($functionName);
+        $ast->doExec('extract', $this->getScopeVariable());
+        $ast->doIf(BH::hexpr(BH::hvar('return'), '==', true));
+        $ast->doExec('ob_start');
+        $ast->doEndif();
+
+        $this->generateOpCode($parsed, $ast);
+
+        if ($this->baseTemplate)
+        {
+            $expr = $this->exprCallBaseTemplate();
+
+            $this->doPrint($ast, $expr);
+        }
+
+        $ast->doIf(BH::hexpr(BH::hvar('return'), '==', true));
+        $ast->doReturn(BH::hexec('ob_get_clean'));
+        $ast->doEndif();
+        $ast->doEndfunction();
+
+        if ($this->checkFunction)
+        {
+            $ast->doEndif();
+        }
+
+        $opCode = $ast->getArray(true);
+
+        $output = $this->generator->getCode($opCode, $this->getScopeVariable(null, true));
+
+        if (! empty($this->debugFile))
+        {
+            $opCode['php'] = $output;
+
+            file_put_contents($this->debugFile, print_r($opCode, true), LOCK_EX);
+        }
+
+        return $output;
     }
 
 }
