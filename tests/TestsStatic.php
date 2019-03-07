c5e37a90e2e97720145a074c361a3c154bd8e39b
S.Zares <sergiozares@gmail.com>
2019-03-07 18:59:33 +0200

    KHBBN15LSXIDADYWBI8DWVSW

--- tests/TestsStatic.php
+++ tests/TestsStatic.php
@@ -4,29 +4,29 @@ namespace placer\brio\tests;
 
 class TestsStatic
 {
-	/**
-	 * Static data
-	 * @var array
-	 */
-	public static $fooBar = ['foo','bar'];
+    /**
+     * Static data
+     * @var array
+     */
+    public static $fooBar = ['foo','bar'];
 
-	/**
-	 * Get Data
-	 * @param  integer $item
-	 * @return string
-	 */
-	public static function getData($item = 0): string
-	{
-		if ($data = static::$fooBar[$item]);
-			return $data;
+    /**
+     * Get Data
+     * @param  integer $item
+     * @return string
+     */
+    public static function getData($item = 0): string
+    {
+        if ($data = static::$fooBar[$item]);
+            return $data;
 
-		return 'Not Found!';
-	}
+        return 'Not Found!';
+    }
 
-	/**
-	 * Self class name
-	 * @return string
-	 */
+    /**
+     * Self class name
+     * @return string
+     */
     public function __invoke()
     {
         return __CLASS__;
